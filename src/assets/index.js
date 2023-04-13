17082693cea67f185f6476a41f45ea1cb692fe12
benholness <benholness@hotmail.co.uk>
2023-04-13 22:26:46 +0100

    feat: add temp company logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,3 +4,5 @@ export { default as moon } from './Moon.svg';
 export { default as hamburger } from './Hamburger.svg';
 export { default as plus } from './Plus.svg';
 export { default as appleLogo } from './AppleLogo.svg';
+export { default as mailchimpLogo } from './MailchimpLogo.svg';
+export { default as gitlabLogo } from './GitlabLogo.svg';
