d0004905a73b1160be1d5443b1131dc0fcc67f4c
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-15 13:54:15 +0100

    Fixing design

--- src/components/FeaturedCompanies.jsx
+++ src/components/FeaturedCompanies.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
+import { FeaturedCompanyCard } from '.';
+import { appleLogo, mailchimpLogo, gitlabLogo } from '../assets';
 
 const FeaturedJobPosts = () => (
   <div>
     <h5>Featured Companies</h5>
+    <div className="flex justify-between">
+      <FeaturedCompanyCard
+        logo={appleLogo}
+        rating={4.5}
+        employerName="Apple Inc."
+        location="New York, USA"
+        vacancies={5}
+        reviewCount={19000}
+      />
+      <FeaturedCompanyCard
+        logo={mailchimpLogo}
+        rating={3.2}
+        employerName="Mailchimp"
+        location="New York, USA"
+        vacancies={1}
+        reviewCount={21500}
+      />
+      <FeaturedCompanyCard
+        logo={gitlabLogo}
+        rating={2.6}
+        employerName="Gitlab"
+        location="New York, USA"
+        vacancies={0}
+        reviewCount={7000}
+      />
+    </div>
+
+    {/* <div className="grid grid-cols-3 w-full">
+      <div className="flex flex-col md:flex-row gap-8 col-span-2">
+        <FeaturedCompanyCard
+          logo={appleLogo}
+          rating={4.5}
+          employerName="Apple Inc."
+          location="New York, USA"
+          vacancies={5}
+          reviewCount={19000}
+        />
+        <FeaturedCompanyCard
+          logo={mailchimpLogo}
+          rating={3.2}
+          employerName="Mailchimp"
+          location="New York, USA"
+          vacancies={1}
+          reviewCount={21500}
+        />
+        <FeaturedCompanyCard
+          logo={gitlabLogo}
+          rating={2.6}
+          employerName="Gitlab"
+          location="New York, USA"
+          vacancies={0}
+          reviewCount={7000}
+        />
+      </div>
+    </div> */}
   </div>
 );
 
--- src/components/FeaturedCompanyCard.jsx
+++ src/components/FeaturedCompanyCard.jsx
@@ -3,33 +3,46 @@ import Image from 'next/image';
 import { BriefcaseIcon, PinIcon, RatingStars } from './icons';
 import Button from './Button';
 
-const FeaturedCompanyCard = ({ logo, rating = 3, employerName = 'Company', location = 'Neverland', vacancies = 10, reviewCount = 10000 }) => {
+const FeaturedCompanyCard = ({
+  logo,
+  rating = 3,
+  employerName = 'Company',
+  location = 'Neverland',
+  vacancies = 10,
+  reviewCount = 10000,
+}) => {
   const formattedReviewCount = `${(reviewCount / 1000).toFixed(1)}k`;
 
   return (
-    <div className="bg-white p-4 rounded-xl flex flex-col space-y-8 w-full">
+    <div className="bg-white p-4 rounded-xl flex flex-col space-y-8 w-full mr-3">
       <div className="flex items-center space-x-3">
-        <Image
-          src={logo}
-          width={60}
-          height={60}
-          alt={employerName}
-        />
+        <Image src={logo} width={60} height={60} alt={employerName} />
         <div className="flex flex-col space-y-1">
           <div className="font-bold text-xl">{employerName}</div>
           <div className="flex items-center space-x-2">
             <div className="flex">
               <RatingStars rating={rating} />
             </div>
-            <div className="text-natural-2 font-bold text-base">{formattedReviewCount}</div>
+            <div className="text-natural-2 font-bold text-base">
+              {formattedReviewCount}
+            </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col space-y-2">
-        <div className="flex gap-2 text-natural-2 font-bold"><PinIcon />{location}</div>
-        <div className="flex gap-2 text-natural-2 font-bold"><BriefcaseIcon />{vacancies === 0 ? 'No' : vacancies} Job {vacancies === 1 ? 'Vacancy' : 'Vacancies'}</div>
+        <div className="flex gap-2 text-natural-2 font-bold">
+          <PinIcon />
+          {location}
+        </div>
+        <div className="flex gap-2 text-natural-2 font-bold">
+          <BriefcaseIcon />
+          {vacancies === 0 ? 'No' : vacancies} Job{' '}
+          {vacancies === 1 ? 'Vacancy' : 'Vacancies'}
+        </div>
       </div>
-      <Button secondary fullWidth>See All</Button>
+      <Button secondary fullWidth>
+        See All
+      </Button>
     </div>
   );
 };
