3ff0c7837d4462ae1fd40d7ce44b59cc5ca253a9
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-15 13:59:22 +0100

    Removed unused old code

--- src/components/FeaturedCompanies.jsx
+++ src/components/FeaturedCompanies.jsx
@@ -31,35 +31,6 @@ const FeaturedJobPosts = () => (
         reviewCount={7000}
       />
     </div>
-
-    {/* <div className="grid grid-cols-3 w-full">
-      <div className="flex flex-col md:flex-row gap-8 col-span-2">
-        <FeaturedCompanyCard
-          logo={appleLogo}
-          rating={4.5}
-          employerName="Apple Inc."
-          location="New York, USA"
-          vacancies={5}
-          reviewCount={19000}
-        />
-        <FeaturedCompanyCard
-          logo={mailchimpLogo}
-          rating={3.2}
-          employerName="Mailchimp"
-          location="New York, USA"
-          vacancies={1}
-          reviewCount={21500}
-        />
-        <FeaturedCompanyCard
-          logo={gitlabLogo}
-          rating={2.6}
-          employerName="Gitlab"
-          location="New York, USA"
-          vacancies={0}
-          reviewCount={7000}
-        />
-      </div>
-    </div> */}
   </div>
 );
 
