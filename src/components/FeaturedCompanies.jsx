1acfe4c4068aed1b5242b0eebc2ccf8ef05a2e02
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-17 14:13:16 +0100

    Removed unused code

--- src/components/FeaturedCompanies.jsx
+++ src/components/FeaturedCompanies.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FeaturedCompanyCard } from '.';
-import { appleLogo, mailchimpLogo, gitlabLogo } from '../assets';
 import { FeaturedCompanies } from '../samples/static-data';
 
 const FeaturedJobPosts = () => (
@@ -18,30 +17,6 @@ const FeaturedJobPosts = () => (
           reviewCount={company.reviewCount}
         />
       ))}
-      {/* <FeaturedCompanyCard
-        logo={appleLogo}
-        rating={4.5}
-        employerName="Apple Inc."
-        location="New York, USA"
-        vacancies={5}
-        reviewCount={19000}
-      />
-      <FeaturedCompanyCard
-        logo={mailchimpLogo}
-        rating={3.2}
-        employerName="Mailchimp"
-        location="New York, USA"
-        vacancies={1}
-        reviewCount={21500}
-      />
-      <FeaturedCompanyCard
-        logo={gitlabLogo}
-        rating={2.6}
-        employerName="Gitlab"
-        location="New York, USA"
-        vacancies={0}
-        reviewCount={7000}
-      /> */}
     </div>
   </div>
 );
