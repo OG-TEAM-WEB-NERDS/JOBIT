bb61a6478280d87c658f9dece55388df286af2f3
benholness <benholness@hotmail.co.uk>
2023-04-18 23:04:03 +0100

    feat: company details page components and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import {
   LatestJobPosts,
   RecommendedJobPosts,
-  FeaturedJobPosts,
+  FeaturedCompanies,
   Heading,
 } from '../components';
 import { getDate } from '../utils';
 
 const Dashboard = () => (
-  <section className="py-6 text-center md:text-left">
+  <main className="py-6 text-center md:text-left">
     <Heading heading="Welcome to the Job Search Platform for Developers" />
     {/* <DesignSystem /> */}
 
@@ -15,13 +15,13 @@ const Dashboard = () => (
     <div className="flex flex-col justify-between md:flex-row mt-3 ">
       <div className="flex flex-col md:w-2/3 ">
         <LatestJobPosts />
-        <FeaturedJobPosts />
+        <FeaturedCompanies />
       </div>
       <div className="md:w-1/3 mx-2 px-2 my-3 md:my-0">
         <RecommendedJobPosts />
       </div>
     </div>
-  </section>
+  </main>
 );
 
 export default Dashboard;
