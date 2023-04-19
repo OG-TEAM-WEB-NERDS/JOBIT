8ee16a7f5becfc1702ea7c16c6a54c1d84329abe
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-19 22:31:23 +0100

    Added size prop to Button to customize text size when needed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,21 @@
-const Button = ({ isSubmit, handleClick, primary, secondary, outlined, transparent, children, fullWidth }) => (
+const Button = ({
+  isSubmit,
+  handleClick,
+  primary,
+  secondary,
+  outlined,
+  transparent,
+  children,
+  fullWidth,
+  size,
+}) => (
   <button
     type={isSubmit ? 'submit' : 'button'}
-    className={`group btn ${primary && 'btn-primary'} ${secondary && 'btn-secondary'} ${outlined && 'btn-outlined'} ${transparent && 'btn-transparent'} ${fullWidth && 'w-full justify-center'}`}
+    className={`group btn ${size === 'sm' && 'text-sm'} ${
+      primary && 'btn-primary'
+    } ${secondary && 'btn-secondary'} ${outlined && 'btn-outlined'} ${
+      transparent && 'btn-transparent'
+    } ${fullWidth && 'w-full justify-center'}`}
     onClick={handleClick}
   >
     {children}
