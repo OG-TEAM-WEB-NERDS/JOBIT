a71e79cf9e0816b40bd72f510f454ff723672764
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-19 09:47:29 +0100

    Adding reusable Banner component

---
+++ src/components/shared/Banner.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+const Banner = ({ logo, bannerBg }) => (
+  <section className={`w-full pt-40 ${bannerBg} rounded-t-lg relative`}>
+    <Image
+      src={logo}
+      alt="Employer Logo"
+      width={50}
+      height={50}
+      className="absolute -bottom-6 left-4 rounded-md"
+    />
+  </section>
+);
+
+export default Banner;
