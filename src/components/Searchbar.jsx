2b6352d3a5615b6af03e831090ac6e253c34d748
ConnorHoare <connor@tipcigroup.com>
2023-04-19 11:44:35 +0100

    responsive update on search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { BriefcaseIcon, ChevronIcon, PinIcon, SearchIcon } from './icons';
 import Button from './Button';
@@ -8,8 +7,8 @@ const Searchbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { theme } = useTheme();
   return (
-    <div className="flex items-center bg-natural-4 rounded-xl py-3 px-4">
-      <div className="flex ml-2 gap-2 mr-2 w-1/4">
+    <div className="flex flex-col md:flex-row items-center bg-natural-4 rounded-xl py-3 px-4 md:divide-x-2 divide-y-2 md:divide-y-0 h-full justify-between gap-4">
+      <div className="flex ml-2 gap-2 mr-2 w-full md:w-1/4 pt-2 md:pt-0">
         <SearchIcon />
         <input
           type="text"
@@ -17,23 +16,25 @@ const Searchbar = () => {
           className="bg-natural-4 text-black-1 outline-none"
         />
       </div>
-      <div className="flex w-1/2">
-        <button type="button" className="flex justify-between cursor-pointer select-none w-full" onClick={() => setIsOpen((prev) => !prev)}>
-          <PinIcon />
-          <p className="text-natural-2 px-2">Select Location</p>
-          <div className={`justify-end ${isOpen && 'rotate-180'}`}>
-            <ChevronIcon />
-          </div>
+      <div className="flex w-full md:w-1/3 justify-between pt-3 md:pt-0">
+        <button type="button" className="flex cursor-pointer select-none w-full" onClick={() => setIsOpen((prev) => !prev)}>
+          <p className="flex gap-x-2 text-natural-2 md:px-2">
+            <PinIcon />
+            Select Location
+          </p>
         </button>
+        <span className={`self-center ${isOpen && 'rotate-180'}`}>
+          <ChevronIcon />
+        </span>
       </div>
-      <div className="flex px-4 w-1/5">
+      <div className="flex md:px-4 w-full md:w-1/5 pt-3 md:pt-0">
         <div className="mr-2">
           <BriefcaseIcon />
         </div>
         <p className="text-natural-2">Job Type</p>
       </div>
-      <div className="w-1/6 justify-end">
-        <Button primary>Find Job</Button>
+      <div className="justify-end w-full md:w-1/6 pt-4 md:pt-0">
+        <Button primary fullWidth>Find Job</Button>
       </div>
     </div>
   );
