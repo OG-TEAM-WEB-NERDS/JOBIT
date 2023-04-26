98cdb04c40eb747080633420a692b092da41f6ca
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-26 19:05:38 +0100

    Updated config to use images from any host

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'encrypted-tbn0.gstatic.com',
-      'www.chick-fil-a.com',
-      'sparinc.com',
-      'www.lifetime.life',
-      'www.halliburton.com',
-      'via.placeholder.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
 };
