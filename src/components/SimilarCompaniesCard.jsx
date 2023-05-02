26a4e784f1b627e2193d7810b803250f11e00741
benholness <benholness@hotmail.co.uk>
2023-05-02 21:49:15 +0100

    refactor: replace placeholders

--- src/components/SimilarCompaniesCard.jsx
+++ src/components/SimilarCompaniesCard.jsx
@@ -16,9 +16,17 @@ const SimilarCompaniesCard = ({
   return (
     <div className="w-full flex flex-wrap justify-between items-center gap-6 rounded-lg p-4 dark:bg-black-2">
       <div className="flex flex-wrap sm:flex-nowrap gap-4 items-center">
-        <div className="p-1 bg-white rounded-lg">
-          <ImageWrapper src={logo} width={50} height={0} alt={employerName} />
+
+        <div className="flex items-center p-2 bg-natural-4 rounded-xl aspect-square w-16 h-16">
+          <ImageWrapper
+            src={logo}
+            alt={employerName}
+            width={50}
+            height={50}
+            styles="object-contain rounded-lg w-full h-full"
+          />
         </div>
+
         <div className="flex flex-col space-y-2">
           <div className="flex gap-2 items-center flex-wrap">
             <p className="font-semibold">{employerName}</p>
