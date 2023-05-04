efa5b24136c308854da7ec112963cbecb876ad4a
Joseph <jnakarmi123@gmail.com>
2023-05-03 23:56:00 -0400

    Updated Location to be auto complete

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 
 import { BriefcaseIcon, ChevronDownIcon, PinIcon, SearchIcon } from '../assets';
@@ -10,14 +10,25 @@ const Searchbar = ({ setSearch, selection }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [hide, setHide] = useState('hidden');
   const [location, setLocation] = useState('');
+  const [countryList, setCountryList] = useState([]);
   const { theme } = useTheme();
   const inputRef = useRef();
 
-  const selectedOption = (e) => {
-    setHide('hidden');
-    const locale = e.target.innerText;
-    setLocation(locale);
+  //to add a listener to hide Country dropdown on click on window
+  useEffect(() => {
+    return window.addEventListener('click', () => {
+      setHide('hidden');
+    });
+  });
+  const displayOptions = () => {
+    console.log(location);
+    const options = Countires.filter((country) =>
+      country.toLowerCase().includes(location)
+    );
+    setCountryList(options);
+    setHide('');
   };
+
   return (
     <div className="flex flex-col md:flex-row items-center bg-natural-4 dark:bg-black-2 rounded-xl py-3 px-4 md:divide-x-2 divide-y-2 md:divide-y-0 dark:divide-black-3 justify-between gap-4 my-10">
       <div className="flex  gap-2 mr-2 w-full md:w-1/4 pt-2 md:pt-0">
@@ -38,7 +49,10 @@ const Searchbar = ({ setSearch, selection }) => {
           ref={inputRef}
         />
       </div>
-      <div className="relative flex w-full md:w-1/3 justify-between pt-3 md:pt-0">
+      <div
+        className="relative flex w-full md:w-1/3 justify-between pt-3 md:pt-0"
+        onClick={() => event.stopImmediatePropagation()}
+      >
         <div className="flex flex-col w-full">
           <div className="flex flex-row w-full justify-between">
             <button
@@ -57,10 +71,10 @@ const Searchbar = ({ setSearch, selection }) => {
                 <input
                   placeholder="Select country name..."
                   className="bg-natural-4 dark:bg-black-2 text-black-1 outline-none dark:text-natural-2"
-                  onClick={() => setHide('hidden')}
-                  onKeyUp={() => setHide('')}
-
-                  //value={location}
+                  //onClick={() => setHide('hidden')}
+                  onChange={(e) => setLocation(e.target.value)}
+                  onKeyUp={() => displayOptions()}
+                  value={location}
                 />
               </div>
             </button>
@@ -76,18 +90,21 @@ const Searchbar = ({ setSearch, selection }) => {
           </div>
           {/* Dropdown list for Countries */}
           <div
-            className={`absolute mt-12 h-60 overflow-y-auto bg-white  dark:bg-gray-700 rounded-md w-full justify-between pt-3 md:pt-0 ${hide}`}
+            className={`absolute mt-12 h-40 overflow-y-auto bg-white  dark:bg-gray-700 rounded-md w-full justify-between pt-3 md:pt-0 ${hide}`}
           >
             <ul
               className="py-2 text-sm text-gray-700 dark:text-gray-200"
               aria-labelledby="dropdownHoverButton"
             >
-              {Countires.map((items, i) => (
+              {countryList.map((items, i) => (
                 <li key={i}>
                   <a
                     href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    onClick={(e) => selectedOption(e)}
+                    className="block px-4 py-2  hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    onClick={() => {
+                      setLocation(items);
+                      setHide('hidden');
+                    }}
                   >
                     {items}
                   </a>
